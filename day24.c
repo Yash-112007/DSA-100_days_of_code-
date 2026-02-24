df600a621ce203ab86d4b1109aa329b3c20ccee9
Shreyansh <yash48588@gmail.com>
2026-02-24 19:27:57 +0530

    Create day24.c

---
+++ day24.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+
+// create linked list
+struct Node* createList(int n)
+{
+    struct Node *head = NULL, *temp = NULL, *newNode;
+    int value;
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if(head == NULL)
+        {
+            head = newNode;
+            temp = newNode;
+        }
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    return head;
+}
+
+
+// delete first occurrence
+struct Node* deleteKey(struct Node* head, int key)
+{
+    struct Node *temp = head, *prev = NULL;
+
+    // if first node itself is key
+    if(temp != NULL && temp->data == key)
+    {
+        head = temp->next;
+        free(temp);
+        return head;
+    }
+
+    // search key
+    while(temp != NULL && temp->data != key)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    // if key found
+    if(temp != NULL)
+    {
+        prev->next = temp->next;
+        free(temp);
+    }
+
+    return head;
+}
+
+
+// print list
+void printList(struct Node* head)
+{
+    while(head != NULL)
+    {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+
+int main()
+{
+    int n, key;
+
+    scanf("%d", &n);
+
+    struct Node* head = createList(n);
+
+    scanf("%d", &key);
+
+    head = deleteKey(head, key);
+
+    printList(head);
+
+    return 0;
+}
