508575f5277f07de75ba0db95bf9616cd6cead85
Shreyansh <yash48588@gmail.com>
2026-03-09 14:43:45 +0530

    Create day37.c

---
+++ day37.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+int pq[MAX];
+int size = 0;
+
+// Insert element
+void insert(int x)
+{
+    if(size == MAX)
+    {
+        return;
+    }
+
+    int i = size - 1;
+
+    // Shift elements to maintain sorted order
+    while(i >= 0 && pq[i] > x)
+    {
+        pq[i + 1] = pq[i];
+        i--;
+    }
+
+    pq[i + 1] = x;
+    size++;
+}
+
+// Delete highest priority element
+int deletePQ()
+{
+    if(size == 0)
+        return -1;
+
+    int val = pq[0];
+
+    for(int i = 0; i < size - 1; i++)
+        pq[i] = pq[i + 1];
+
+    size--;
+
+    return val;
+}
+
+// Peek element
+int peek()
+{
+    if(size == 0)
+        return -1;
+
+    return pq[0];
+}
+
+int main()
+{
+    int n, x;
+    char op[10];
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%s", op);
+
+        if(strcmp(op, "insert") == 0)
+        {
+            scanf("%d", &x);
+            insert(x);
+        }
+        else if(strcmp(op, "delete") == 0)
+        {
+            printf("%d\n", deletePQ());
+        }
+        else if(strcmp(op, "peek") == 0)
+        {
+            printf("%d\n", peek());
+        }
+    }
+
+    return 0;
+}
