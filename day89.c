064673d0d552d2a89d9b03a6bc96445fa8cf1b86
Shreyansh <yash48588@gmail.com>
2026-04-30 23:47:30 +0530

    Create day89.c

---
+++ day89.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int isPossible(int arr[], int n, int m, int maxPages) {
+    int students = 1;
+    int pagesSum = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > maxPages)
+            return 0;
+
+        if(pagesSum + arr[i] > maxPages) {
+            students++;
+            pagesSum = arr[i];
+            if(students > m)
+                return 0;
+        } else {
+            pagesSum += arr[i];
+        }
+    }
+    return 1;
+}
+
+int findMinPages(int arr[], int n, int m) {
+    int sum = 0, maxBook = 0;
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+        if(arr[i] > maxBook) maxBook = arr[i];
+    }
+
+    int low = maxBook, high = sum, result = sum;
+
+    while(low <= high) {
+        int mid = (low + high) / 2;
+
+        if(isPossible(arr, n, m, mid)) {
+            result = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return result;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    if(m > n) {
+        printf("-1");
+        return 0;
+    }
+
+    printf("%d", findMinPages(arr, n, m));
+    return 0;
+}
